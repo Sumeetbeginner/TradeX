44cd32ed4ef8ed8176fe7f19fa63284aa5ea924e
Sumeet Gupta <sumeetgupta3690@gmail.com>
2025-04-22 20:10:04 +0530

    Upd

--- server/Algorithms/News/scrapenews.py
+++ server/Algorithms/News/scrapenews.py
@@ -7,20 +7,25 @@ def fetch_stock_news():
     response.raise_for_status()
     
     soup = BeautifulSoup(response.content, 'html.parser')
-    links = soup.find_all('a', class_='lst_lnk_img')
-    
     news_items = []
-    for link in links:
-        title = link.get('title')
-        href = link.get('href')
-        img_tag = link.find('img')
-        img_src = img_tag.get('data-src') if img_tag.get('data-src') else img_tag.get('src')
-        
-        if title and href and img_src:
-            news_items.append({
-                'title': title,
-                'href': href,
-                'img_src': img_src
-            })
-    
+
+    # Locate the <li> items containing the news
+    for item in soup.find_all('li', class_='lst_li'):
+        try:
+            img_tag = item.find('a', class_='lst_lnk_img').find('img')
+            img_src = img_tag.get('data-src') or img_tag.get('src')
+
+            title_tag = item.find('a', class_='lst_lnk_txt')
+            title = title_tag.text.strip()
+            href = title_tag.get('href')
+
+            if title and href and img_src:
+                news_items.append({
+                    'title': title,
+                    'href': href,
+                    'img_src': img_src
+                })
+        except AttributeError:
+            continue  # Skip malformed or incomplete items
+
     return news_items
